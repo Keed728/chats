546d375820bc4988dba9ebb5895bb3521c59a375
Keed728 <keerthana728driya@gmail.com>
2024-11-10 14:13:42 +0530

    Updated App.java

--- src/main/java/com/nokia/App.java
+++ src/main/java/com/nokia/App.java
@@ -6,5 +6,6 @@ package com.nokia;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("good morning");
     }
 }
